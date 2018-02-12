4e39c6169e45e3c12d28c3ae4777961d6a9bdddd
Alessandro Baffa <alessandro.baffa@gmail.com>
2018-02-12 09:59:48 +0900

    first commit

---
+++ handlers.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Welcome!\n")
+}
+
+func PrintCV(w http.ResponseWriter, r *http.Request) {
+	cv := CV{
+		Name:       "Alessandro Baffa",
+		Profession: "Senior Software Engineer",
+		City:       "Tokyo, Japan",
+		Contacts: map[string]string{
+			"phone":    "(+81)080-2556-1845)",
+			"email":    "contact@alebaffa.com",
+			"blog":     "alebaffa.com",
+			"twitter":  "@alebaffa",
+			"linkedIn": "in/alessandrobaffa"}}
+
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(cv); err != nil {
+		panic(err)
+	}
+}
